7ca2aa202713b2d82b3ad856e47229c2f4d2b403
mdashikulislam <ashik.nwu@gmail.com>
2024-07-28 04:22:03 +0600

    initial file

---
+++ app/_layout.jsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Slot,Stack } from 'expo-router';
+function RootLayout() {
+  return (
+    <>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text><Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text><Text>Open up App.js to start working on your app!</Text>
+      
+      <StatusBar style="auto" />
+    </>
+  );
+}
+export default RootLayout;
