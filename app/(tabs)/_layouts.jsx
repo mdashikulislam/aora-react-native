8f52ed1894133a2dcc4a801df6b5718e1226fa03
mdashikulislam <ashik.nwu@gmail.com>
2024-07-28 05:42:17 +0600

    update

--- app/(tabs)/_layouts.jsx
+++ app/(tabs)/_layouts.jsx
@@ -1,11 +1,37 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs,Redirect } from 'expo-router'
-const TabsLayout = () => {
+import { View,Image } from 'react-native'
+import {icons} from '../../constants'
+const TabIcon = ({icon,color,name,focused}) =>{
   return (
     <View>
-      <Text>TabsLayout</Text>
+      <Image source={icon} resizeMethod='contain' tintColor={color}  className="w-6 h-6"/>
+      <Text className={`${focused ? 'font-psemibold' :'font-pregular'} text-xs`}>{name}</Text>
     </View>
+  )
+}
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs>
+        <Tabs.Screen 
+          name='home'
+          options={{
+            title:'Home',
+            headerShown:false,
+            tabBarIcon:({ color,focused }) => (
+              <TabIcon 
+                icon={icons.home} 
+                color={color}
+                name="Home"
+                focused={focused}  
+              />
+            )
+
+          }}
+          />
+      </Tabs>
+    </>
   )
 }
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,14 @@
+
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Apps() {
   return (
-    <View style={styles.container}>
-      
-    <StatusBar style="auto" />
-  </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora</Text>
+      <StatusBar style='auto'/>
+      <Link href="/home" style={{color:'red'}}>Go to Home</Link>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
